b9e5918e17c8a4a33636db7c9717520b9632a8cc
AmitKumar1996 <kumaramit1996.1@gmail.com>
2025-04-27 23:27:33 +0530

    EmployeeAPI_04_28

---
+++ EmployeeAPI_04_28/src/main/java/dev/amit/EmployeeAPI_04_28/EmployeeApi0428Application.java
@@ -0,0 +1,13 @@
+package dev.amit.EmployeeAPI_04_28;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeApi0428Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeApi0428Application.class, args);
+	}
+
+}
---
+++ EmployeeAPI_04_28/src/test/java/dev/amit/EmployeeAPI_04_28/EmployeeApi0428ApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.amit.EmployeeAPI_04_28;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeApi0428ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
