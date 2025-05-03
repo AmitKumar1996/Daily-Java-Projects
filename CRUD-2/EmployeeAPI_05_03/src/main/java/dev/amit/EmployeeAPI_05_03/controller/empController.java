1c44b59b7cd73e23a2f4d5a9272490e1f862fc80
AmitKumar1996 <kumaramit1996.1@gmail.com>
2025-05-03 23:57:56 +0530

    Employee_05_03

--- CRUD-2/EmployeeAPI_05_03/src/main/java/dev/amit/EmployeeAPI_05_03/controller/empController.java
+++ CRUD-2/EmployeeAPI_05_03/src/main/java/dev/amit/EmployeeAPI_05_03/controller/empController.java
@@ -5,6 +5,7 @@ import java.net.ResponseCache;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,7 +22,7 @@ public class empController {
 	empRepository empRepository;
 	
 	
-	@PutMapping("/employee")
+	@PostMapping("/employee")
 	public ResponseEntity<?> createUser(@RequestBody Employee employee){
 		
 		empRepository.save(employee);
